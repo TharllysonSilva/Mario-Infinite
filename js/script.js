2610d44061c78e515f1ef8fff290cfe604d13f6f
Tarllysson Santos <tharllysonsilva@gmail.com>
2022-06-02 14:58:17 -0300

    animation conclusão

--- js/script.js
+++ js/script.js
@@ -14,12 +14,22 @@ const jump =() => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    
-    if(pipePosition <= 120){
+    const marioPosition =  +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
         pipe.style.animation = 'nome';
-        pipe.stle.left = `${pipePosition}px`;
+        pipe.style.left = `${pipePosition}px`;
 
+        mario.style.animation = 'nome';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './Imagens/game-over.png';
+        mario.style.width ='75px'
+        mario.style.marginLeft ='50px'
+
+        clearInterval(loop);
+        
     }
 
 }, 10);
