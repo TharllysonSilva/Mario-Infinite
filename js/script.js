67449dbcb11ebb3be217be47939e3d6ff58f658b
Tarllysson Santos <tharllysonsilva@gmail.com>
2022-06-02 12:25:49 -0300

    error border

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump =() => {
     mario.classList.add('jump');
 
-    setTimeout(() => {
+    setTimeout(() => {  
 
         mario.classList.remove('jump');
-
+        
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    
+    if(pipePosition <= 120){
+
+        pipe.style.animation = 'nome';
+        pipe.stle.left = `${pipePosition}px`;
+
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump);
